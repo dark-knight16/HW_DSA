892e46b0b023c0c843ce5a993205e0e14636d47f
dark-knight16 <ledangquang16022005@gmail.com>
2024-10-13 15:29:20 +0700

    Add function calculate address of array two dimensions

--- Week_5_Q2.cpp
+++ Week_5_Q2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int prime[1000001];
@@ -10,7 +9,7 @@ void sieve_Eratosthenes() {
         prime[i] = 1;
     }
     prime[0] = prime[1] = 0;
-    for (int i = 2; i <= sqrt(1000000); i++) {
+    for (int i = 2; i <= 1000; i++) {
         if (prime[i]) {
             for (int j = i * i; j <= 1000000; j += i) prime[j] = 0;
         }
